80ecd1bfbf863efa75f967e57de4167937628f2b
Georg Busch <georgb99@gmail.com>
2021-01-11 12:14:33 +0100

    ++

--- src/svenGeorgMain/TranslationClassGoogle.java
+++ src/svenGeorgMain/TranslationClassGoogle.java
@@ -5,56 +5,119 @@ import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
 
 public class TranslationClassGoogle {
+	
 	static Scanner scanner = new Scanner(System.in);
 	
-	
-	public static void translateText() {
-		String sourceLang = getSourceLang();
+	public static String translateText() {
+		String sourceLanguage = getSourceLanguage();
+		String targetLanguage = getTargetLanguage();
 		String userInput = getInput();
-		String targetLang = getTargetLang();
-		TranslateTheString(userInput, sourceLang, targetLang);
+		//translateInputString(userInput, sourceLanguage, targetLanguage);
+		String translatedInput = translateInputString(userInput, sourceLanguage, targetLanguage);
+		System.out.println("Translated text is : " + translatedInput);
+		
+		return translatedInput;
+	}
+	
+	public static String getSourceLanguage() {
+		System.out.println("Enter the source language  (type \"help\" for a list of language often used ISO-639-1-Codes):");
+		String sourceLanguage = "";
+		sourceLanguage = scanner.nextLine();
+		
+		if (sourceLanguage.equals("help")) {
+			System.out.println(
+					"German: "+"de"+"\n"+
+					"Spanish: "+"es"+"\n"+
+					"French: "+"fr"+"\n"+
+					"English: "+"en"+"\n"+
+					"Russian: "+"ru"+"\n"+
+					"Norwegian: "+"no"+"\n"+
+					"Chinese: "+"zh"+"\n"+
+					"Japanese: "+"ja"+"\n"+
+					"Korean: "+"ko"+"\n" +
+					"Turkish: " +"tr"
+			);
+			System.out.println("Enter the source language:");
+			sourceLanguage = scanner.nextLine();
+			while (sourceLanguage.equals("")) {
+				System.out.println("Please enter something!");
+				sourceLanguage = scanner.nextLine();
+			}
+			return sourceLanguage;
+		} else {
+			System.out.println();
+			while (sourceLanguage.equals("")) {
+				System.out.println("Please enter something!");
+				sourceLanguage = scanner.nextLine();
+			}
+			return sourceLanguage;
+		}
+	}
+	
+	public static String getTargetLanguage() {
+		System.out.println("Enter the target language (type \"help\" for a list of language often used ISO-639-1-Codes):");
+		String targetLanguage = "";
+		targetLanguage = scanner.nextLine();
+		
+		if (targetLanguage.equals("help")) {
+			System.out.println(
+					"German: "+"de"+"\n"+
+					"Spanish: "+"es"+"\n"+
+					"French: "+"fr"+"\n"+
+					"English: "+"en"+"\n"+
+					"Russian: "+"ru"+"\n"+
+					"Norwegian: "+"no"+"\n"+
+					"Chinese: "+"zh"+"\n"+
+					"Japanese: "+"ja"+"\n"+
+					"Korean: "+"ko"+"\n" +
+					"Turkish: " +"tr"
+			);
+			System.out.println("Enter the target language:");
+			targetLanguage = scanner.nextLine();
+			while (targetLanguage.equals("")) {
+				System.out.println("Please enter something!");
+				targetLanguage = scanner.nextLine();
+			}
+			return targetLanguage;
+		} else {
+			System.out.println();
+			while (targetLanguage.equals("")) {
+				System.out.println("Please enter something!");
+				targetLanguage = scanner.nextLine();
+			}
+			return targetLanguage;
+		}
 	}
 	
 	public static String getInput() {
-		System.out.println("Enter your text and end your input with 'end':");
+		System.out.println("Enter your text and end your input with '/end/' to submit your input:");
 		String userInput = "";
-		String theInput;
+		String partOfInput;
 		while(scanner.hasNextLine()) {
-			theInput=scanner.nextLine();
-			if(theInput.equals("end")) {
+			partOfInput=scanner.nextLine();
+			if(partOfInput.equals("/end/")) {
 				break;
 			}
-			userInput += " " + "\n" + theInput;	
+			userInput += " " + "\n" + partOfInput;	
 		}
-		System.out.println("Input: " + userInput);
+		System.out.println("Your input was: " + userInput);
+		System.out.println();
 		return userInput;
 	}
-	public static String getSourceLang() {
-		System.out.println("\n");
-		System.out.println("Enter the source Language:");
-		String sourceLang = "";
-		sourceLang = scanner.nextLine();
-		return sourceLang;
-	}
-	public static String getTargetLang() {
-		System.out.println("\n");
-		System.out.println("Enter the target Language:");
-		String targetLang = "";
-		targetLang = scanner.nextLine();
-		return targetLang;
-	}
-	public static void TranslateTheString(String text, String sourceL, String targetL) {
+	
+	public static String translateInputString(String inputString, String sourceLanguage, String targetLanguage) {
 		Translate translate = TranslateOptions.getDefaultInstance().getService();
 
-		Translation translation =
-			    translate.translate(
-			        text,
-			        Translate.TranslateOption.sourceLanguage(sourceL),
-			        Translate.TranslateOption.targetLanguage(targetL),
+		Translation translation = translate.translate(
+				
+			        inputString,
+			        Translate.TranslateOption.sourceLanguage(sourceLanguage),
+			        Translate.TranslateOption.targetLanguage(targetLanguage),
 			        // Use "base" for standard edition, "nmt" for the premium model.
-			        Translate.TranslateOption.model("base"));
-
-			System.out.printf("TranslatedText:\nText: %s\n", translation.getTranslatedText());
+			        Translate.TranslateOption.model("base")
+		);
+			
+		return translation.getTranslatedText();
 	}
 	
 }
